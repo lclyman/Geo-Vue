7ca9f6c0b689a527dfebed53d9d6e8fdf92ca541
Larry Clyman <lclyman@gmail.com>
2018-06-10 09:10:47 -0600

    Update init.js

--- src/firebase/init.js
+++ src/firebase/init.js
@@ -2,17 +2,11 @@ import firebase from 'firebase'
 import firestore from 'firebase/firestore'
 
 var config = {
-  apiKey: "AIzaSyDUPc2at3SDgbhC0UrjJVTRvOvSUMeT7Pg",
-  authDomain: "smoothies-99a52.firebaseapp.com",
-  databaseURL: "https://smoothies-99a52.firebaseio.com",
-  projectId: "smoothies-99a52",
-  storageBucket: "smoothies-99a52.appspot.com",
-  messagingSenderId: "185959016167"
+// << your firebase config >>
 };
 
 const firebaseApp = firebase.initializeApp(config);
 // firebaseApp.firestore().settings({ timestampsInSnapshots: true })
 
 export default firebaseApp.firestore()
-
 
